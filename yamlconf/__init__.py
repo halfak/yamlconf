3fab40a76555645b2179fc4502815f7bac2d1ece
halfak <aaron.halfaker@gmail.com>
2016-08-30 13:45:57 -0500

    Increments version to 0.2.0

--- yamlconf/__init__.py
+++ yamlconf/__init__.py
@@ -1,8 +1,9 @@
 from .import_module import import_module
+from .import_path import import_path
 from .load import load, loads
 from .merge import merge
 from .propagate_defaults import propagate_defaults
 
-__all__ = [import_module, load, loads, merge, propagate_defaults]
+__all__ = [import_module, import_path, load, loads, merge, propagate_defaults]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
