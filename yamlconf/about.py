041765b76e0ee0b0abd7d028b850892002a1ae94
Ilias Sarantopoulos <isaranto@users.noreply.github.com>
2023-01-23 18:14:17 +0200

    Bump version

--- yamlconf/about.py
+++ yamlconf/about.py
@@ -1,5 +1,5 @@
 __name__ = "yamlconf"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "Aaron Halfaker"
 __author_email__ = "aaron.halfaker@gmail.com"
 __description__ = "Yaml-based configuration file reader."
