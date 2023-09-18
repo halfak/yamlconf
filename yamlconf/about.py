7e66f6b9d309cce3d20034449fe25716630e123b
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2023-09-18 15:50:10 +0300

    bump YAML to 6.0.1
    Bug: T346446

--- yamlconf/about.py
+++ yamlconf/about.py
@@ -1,10 +1,11 @@
-__name__ = "yamlconf"
-__version__ = "0.2.6"
+__name__ = "yamlconf-wmf"
+__version__ = "0.2.7"
 __author__ = "Aaron Halfaker"
 __author_email__ = "aaron.halfaker@gmail.com"
-__description__ = "Yaml-based configuration file reader."
+__description__ = "Yaml-based configuration file reader. This is a fork of the original yamlfconf package. " \
+                  "This fork is maintained by WMF Machine Learning Team"
 __license__ = "MIT"
-__url__ = "https://github.com/halfak/yamlconf"
+__url__ = "https://github.com/wikimedia/yamlconf"
 
 all = [__name__, __version__, __author__, __author_email__, __description__,
        __license__, __url__]
